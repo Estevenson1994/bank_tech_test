d9ec9f92afc8247c27179aaccc577fcb00b59c6d
Erin Stevenson <estevenson_94@live.com>
2019-03-11 18:36:30 +0000

    Statement can print one deposit

--- lib/statement.rb
+++ lib/statement.rb
@@ -8,18 +8,31 @@ class Statement
     @transactions = []
   end
 
-
   def store_transaction(transaction)
     @transactions << transaction
+  end
+
+  def print_statement
+    statement = "date || credit || debit || balance"
+    @transactions.each do |transaction| 
+      statement += "\n" + formatted_date(transaction.date)
+      if transaction.type == :deposit
+        statement += " || " + '%.2f' % transaction.amount + " ||"
+      else
+        statement += " || || " + '%.2f' % transaction.amount + " || "
+      end
+      statement += " || " + '%.2f' % transaction.balance
+    end
+    statement
   end
 
   private
 
   def formatted_date(date)
     if date.is_a? String
-      Date.parse(date).strftime("%d/%m/%y")
+      Date.parse(date).strftime("%d/%m/%Y")
     else 
-      date.strftime("%d/%m/%y")
+      date.strftime("%d/%m/%Y")
     end
   end
 end
--- spec/units/statement_spec.rb
+++ spec/units/statement_spec.rb
@@ -2,8 +2,8 @@ require 'statement'
 
 RSpec.describe Statement do
     before(:each) do
-      @transaction1 = instance_double("transaction1", :type => :deposit, :amount => 1000, :date => "12/01/2010", :balance => 1000)
-      @transaction2 = instance_double("transaction2", :type => :withdrawal, :amount => 300, :date => "13/01/2010", :balance => 700)
+      @transaction1 = instance_double("transaction1", :type => :deposit, :amount => 1000.00, :date => "12/01/2010", :balance => 1000)
+      @transaction2 = instance_double("transaction2", :type => :withdrawal, :amount => 300.00, :date => "13/01/2010", :balance => 700)
       @statement = Statement.new
     end
 
@@ -14,5 +14,9 @@ RSpec.describe Statement do
   it 'can store transactions' do
     expect{ @statement.store_transaction(@transaction1) }.to change{ @statement.transactions.length}.by(1)
   end
-    
+
+  it 'can print statement' do
+    @statement.store_transaction(@transaction1)
+    expect(@statement.print_statement).to eq("date || credit || debit || balance\n12/01/2010 || 1000.00 || || 1000.00")
+  end
 end
