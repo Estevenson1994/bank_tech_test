4fc8d5cc6cf2b60627f6cbe799c4aa44dd7bdb11
Erin Stevenson <estevenson_94@live.com>
2019-03-11 13:56:43 +0000

    Encapulate formattedDate method

--- lib/statement.rb
+++ lib/statement.rb
@@ -8,16 +8,23 @@ class Statement
 
   def deposit(amount, date = Date.today)
     @balance += amount
-    if date.is_a? String
-      formatted_date = Date.parse(date).strftime("%d/%m/%y")
-    else 
-      formatted_date = date.strftime("%d/%m/%y")
-    end
-    @transactions << { date: formatted_date, deposit: amount, balance: @balance }
+    @transactions << { date: formatted_date(date), deposit: amount, balance: @balance }
   end
 
   def withdraw(amount)
     @balance -= amount
     @transactions << { withdraw: amount, balance: @balance }
   end
+
+
+  private
+
+  def formatted_date(date)
+    if date.is_a? String
+      formatted_date = Date.parse(date).strftime("%d/%m/%y")
+    else 
+      formatted_date = date.strftime("%d/%m/%y")
+    end
+  end
+
 end
