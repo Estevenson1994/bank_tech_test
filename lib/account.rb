adaaf8715f650bee59d3adfe1243e4e56eab3a3f
Erin Stevenson <estevenson_94@live.com>
2019-03-11 17:23:21 +0000

    Account deposit and withdraw methods create new transactions and store them on statement

--- lib/account.rb
+++ lib/account.rb
@@ -1,17 +1,18 @@
 class Account
   attr_reader :balance, :transactions
 
-  def initialize
+  def initialize(statement = Statement.new)
+    @statement = statement
     @balance = 0
   end
 
   def deposit(amount, transaction = Transaction, date = Date.today)
-    transaction.new("deposit", amount, date)
+    @statement.transactions << transaction.new("deposit", amount, date)
     @balance += amount
   end
 
   def withdraw(amount, transaction = Transaction, date = Date.today)
-    transaction.new("withdrawal", amount, date)
+    @statement.transactions << transaction.new("withdrawal", amount, date)
     @balance -= amount
   end
 
--- spec/units/account_spec.rb
+++ spec/units/account_spec.rb
@@ -1,18 +1,23 @@
 require 'account'
 
 RSpec.describe Account do
-  subject(:account) { described_class.new }
+  before(:each) do
+    statement = instance_double('statement', :transactions => [])
+
+    @account = Account.new(statement)
+  end
 
   it 'balance is initially 0' do
-    expect(account.balance).to eq(0)
+    expect(@account.balance).to eq(0)
   end
 
   it 'user can make a deposit' do
-    expect { account.deposit(1000) }.to change { account.balance }.by(1000)
+    expect { @account.deposit(1000) }.to change { @account.balance }.by(1000)
   end
 
   it 'user can make a withdrawal' do
-    expect { account.withdraw(500) }.to change { account.balance }.by(-500)
+    expect { @account.withdraw(500) }.to change { @account.balance }.by(-500)
   end
+
 
 end
