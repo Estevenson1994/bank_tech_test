71a8b173a750819fe34d385ab031342c22609ed7
Erin Stevenson <estevenson_94@live.com>
2019-03-11 14:41:36 +0000

    Statement has initially no transactions

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,29 +1,8 @@
 class Statement
-  attr_reader :balance, :transactions
+    attr_reader :transactions
 
   def initialize
-    @balance = 0
     @transactions = []
-  end
-
-  def deposit(amount, date = Date.today)
-    @balance += amount
-    @transactions << { date: formatted_date(date), deposit: amount, balance: @balance }
-  end
-
-  def withdraw(amount, date = Date.today)
-    @balance -= amount
-    @transactions << { date: formatted_date(date), withdraw: amount, balance: @balance }
-  end
-
-  private
-
-  def formatted_date(date)
-    if date.is_a? String
-      Date.parse(date).strftime("%d/%m/%y")
-    else 
-      date.strftime("%d/%m/%y")
-    end
   end
 
 end
--- spec/units/statement_spec.rb
+++ spec/units/statement_spec.rb
@@ -1,40 +1,9 @@
 require 'statement'
 
 RSpec.describe Statement do
-  subject(:statement) { described_class.new }
+    subject(:statement) { described_class.new }
 
-  it 'balance is initially 0' do
-    expect(statement.balance).to eq(0)
-  end
-
-  it 'user can make a deposit' do
-    expect { statement.deposit(1000) }.to change { statement.balance }.by(1000)
-  end
-
-  it 'user can make a withdrawal' do
-    expect { statement.withdraw(500) }.to change { statement.balance }.by(-500)
-  end
-
-  it 'deposits are stored in transactions' do
-    statement.deposit(1000)
-    expect(statement.transactions[0][:deposit]).to eq(1000)
-    expect(statement.transactions[0][:balance]).to eq(1000)
-  end
-
-  it 'withdrawals are stored in transactions' do
-    statement.withdraw(1000)
-    expect(statement.transactions[0][:withdraw]).to eq(1000)
-    expect(statement.transactions[0][:balance]).to eq(-1000)
-  end
-
-  it 'stores the date when a deposit is made' do
-    statement.deposit(1000, "14-01-2012")
-    expect(statement.transactions).to include({ date: "14/01/12", deposit: 1000, balance: 1000 })
-  end
-
-  it 'stores the date when a withdrawal is made' do
-    statement.withdraw(1000, "13-01-2012")
-    expect(statement.transactions).to include({ date: "13/01/12", withdraw: 1000, balance: -1000 })
-  end
-  
+    it 'initially has no transactions' do
+        expect(statement.transactions.length).to eq(0)
+    end
 end
