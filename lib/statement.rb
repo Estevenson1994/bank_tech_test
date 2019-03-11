ffbdf1716a16c0d23e60f588243cb6cffed3ba9b
Erin Stevenson <estevenson_94@live.com>
2019-03-11 15:18:13 +0000

    Statement stores deposit transaction

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,8 +1,15 @@
+
 class Statement
     attr_reader :transactions
 
-  def initialize
+  def initialize(account)
     @transactions = []
+    @account = account
+  end
+
+  def deposit(amount, date = Date.today)
+    @account.deposit(amount, date)
+    @transactions << { date: date, deposit: amount, balance: @account.balance }
   end
 
 end
--- spec/units/statement_spec.rb
+++ spec/units/statement_spec.rb
@@ -1,9 +1,20 @@
 require 'statement'
 
 RSpec.describe Statement do
-    subject(:statement) { described_class.new }
+    before(:each) do
+      account = double('account')
+      allow(account).to receive(:balance)
+      allow(account).to receive(:deposit)
+
+      @statement = Statement.new(account)
+    end
 
     it 'initially has no transactions' do
-        expect(statement.transactions.length).to eq(0)
+        expect(@statement.transactions.length).to eq(0)
+    end
+
+    it 'can store a deposit' do
+        expect { @statement.deposit(1000) }.to change { @statement.transactions.length }.by(1)
+        expect(@statement.transactions[0][:deposit]).to eq(1000)
     end
 end
