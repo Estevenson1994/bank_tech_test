d20621c81371b9ea7d99750431d02b224a72ca5d
Erin Stevenson <estevenson_94@live.com>
2019-03-11 16:38:41 +0000

    Transaction stores date and amount

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,9 +1,13 @@
+require 'date'
+
 class Transaction
 
-    attr_reader :type
+    attr_reader :type, :amount, :date
 
-    def initialize(type)
+    def initialize(type, amount, date = Date.today)
         @type = type
+        @amount = amount
+        @date = date
     end
 
 end
--- spec/units/transaction_spec.rb
+++ spec/units/transaction_spec.rb
@@ -3,11 +3,19 @@ require 'transaction'
 RSpec.describe Transaction do
 
     before(:each) do
-        @transaction = Transaction.new("deposit")
+        @transaction = Transaction.new("deposit", 1000, "12/01/2010")
     end
-
 
     it 'stores transaction type' do
         expect(@transaction.type).to eq("deposit") 
+    end
+
+    it 'stores amount' do
+        expect(@transaction.amount).to eq(1000)
+    end
+
+    it 'stores the date' do
+        expect(@transaction.date).to eq("12/01/2010")
+
     end
 end
